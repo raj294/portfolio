72aa5ab88c50aedcc513b33b7693d3fa6bf55b50
Raj Patel <rajpatel8690@gmail.com>
2021-06-18 19:30:38 +0530

    added netlify form attribute

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,21 +52,21 @@ skillsHeader.forEach((el) => {
 /* QUALIFICATION TABS */
 
 const tabs = document.querySelectorAll('[data-target]'),
-      tabContent = document.querySelectorAll('[data-content]');
+      tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.target);
+        const target = document.querySelector(tab.dataset.target)
 
-        tabContent.forEach(tabContent => {
-            tabContent.classList.remove('qualification__active');
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
         })
-        target.classList.add('qualification__active');
+        target.classList.add('qualification__active')
         
-        tab.forEach(tab => {
-            tab.classList.remove('qualification__active');
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
         })
-        tab.classList.add('qualification__active');
+        tab.classList.add('qualification__active')
     })
 })
 
