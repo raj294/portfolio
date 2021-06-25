d6c3cda490bca168970ad47fa231f788de67e1fc
Raj Patel <rajpatel8690@gmail.com>
2021-06-25 17:41:55 +0530

    added CV and updated the projects cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,7 +96,7 @@ modalCloses.forEach((modalClose) => {
 
 /* SCROLL SECTIONS ACTIVE LINK */
 
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
     const scrollY = window.pageYOffset
@@ -104,15 +104,16 @@ function scrollActive(){
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id');
+        sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
         }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
+window.addEventListener('scroll', scrollActive)
 
 /* CHANGE BACKGROUND HEADER */ 
 function scrollHeader(){
